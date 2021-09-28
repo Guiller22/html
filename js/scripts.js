b983301b97d521e9b712bd720e6606abf0d18c8f
Guiller22 <miito2010v2@hotmail.com>
2021-09-28 11:44:02 +0200

    cambios

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+window.onload=function(){
+    document.getElementById("icon").onclick()=function(){
+    var x = document.getElementById("menu");
+    if (x.className === "menu") {
+        x.className += " responsive";
+    } else {
+        x.className 
+        = "menu";
+    
+}function openNav() {
+    document.getElementById("menu").style.width = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("menu").style.width = "0%";
+  }
+}
+}
